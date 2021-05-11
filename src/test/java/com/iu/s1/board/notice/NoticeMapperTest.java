f6dc9d0cd02a6b371def1ceec2e530ed22231d81
ba-lim <dmsqltkfk276@naver.com>
2021-05-11 23:51:10 +0900

    1

---
+++ src/test/java/com/iu/s1/board/notice/NoticeMapperTest.java
@@ -0,0 +1,15 @@
+package com.iu.s1.board.notice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class NoticeMapperTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
